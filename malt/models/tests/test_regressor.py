2e582334056cfa562fd25077e522e844055cf331
Yuanqing Wang <wangyq@wangyq.net>
2021-03-24 16:19:08 -0400

    rename

--- malt/models/tests/test_regressor.py
+++ malt/models/tests/test_regressor.py
@@ -5,11 +5,25 @@ def test_import():
     from malt.models import regressor
 
 
-def test_construct():
+def test_construct_nn():
     import torch
     import malt
 
     regressor = malt.models.regressor.NeuralNetworkRegressor(
+        in_features=128,
+        out_features=2,
+    )
+    h = torch.zeros(32, 128)
+    theta = regressor(h)
+    assert isinstance(theta, torch.Tensor)
+    assert theta.shape[0] == 32
+    assert theta.shape[1] == 2
+
+def test_construct_gp():
+    import torch
+    import malt
+
+    regressor = malt.models.regressor.ExactGaussianProcessRegressor(
         in_features=128,
         out_features=2,
     )
