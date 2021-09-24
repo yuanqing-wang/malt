e8b1d7be2ef5af0695348f54aa1267362789a4d4
Yuanqing Wang <wangyq@wangyq.net>
2021-09-24 15:27:54 -0400

    merchant base class

--- malt/agents/merchant.py
+++ malt/agents/merchant.py
@@ -9,5 +9,18 @@ class Merchant(abc.ABC):
     catalogue (*args, **kwargs):
         Return an iterator over the catalogue of the merchant.
 
-    
+    order (*args, **kwargs):
+        Place an order from the merchant.
+
     """
+
+    def __init__(self):
+        super(Merchant, self).__init__()
+
+    @abc.abstractmethod
+    def catalogue(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def order(self, *args, **kwargs):
+        raise NotImplementedError
