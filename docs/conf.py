822a265a8a37d8b7ae953b77acb9cb9d6fbbf95a
Yuanqing Wang <wangyq@umich.edu>
2021-09-27 22:58:50 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -93,6 +93,7 @@ pygments_style = 'default'
 # a list of builtin themes.
 #
 # html_theme = 'sphinx_rtd_theme'
+import pytorch_sphinx_theme
 html_theme = 'pytorch_sphinx_theme'
 html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
