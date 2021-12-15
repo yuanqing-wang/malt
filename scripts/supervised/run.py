2d66f2f68d6dba7a3f92adc0d4616bf1651bced9
yuanqing-wang <wangyq@wangyq.net>
2021-12-15 17:10:08 -0500

    scripts

--- scripts/supervised/run.py
+++ scripts/supervised/run.py
@@ -9,15 +9,21 @@ def run(args):
     ds_tr, ds_vl, ds_te = data.split([8, 1, 1])
     model = malt.models.supervised_model.GaussianProcessSupervisedModel(
         representation=malt.models.representation.DGLRepresentation(
-            out_features=32
+            out_features=128
         ),
         regressor=malt.models.regressor.ExactGaussianProcessRegressor(
-            in_features=32, out_features=2,
+            in_features=128, out_features=2,
         ),
         likelihood=malt.models.likelihood.HeteroschedasticGaussianLikelihood(),
     )
-    trainer = malt.trainer.get_default_trainer(without_player=True, n_epochs=1000)
-    model = trainer(ds_tr, model)
+    trainer = malt.trainer.get_default_trainer(without_player=True, n_epochs=3000)
+    model = trainer(model, ds_tr)
+
+    r2 = malt.metrics.supervised_metrics.R2()(model, ds_te)
+    print(r2)
+
+    rmse = malt.metrics.supervised_metrics.RMSE()(model, ds_te)
+    print(rmse)
 
 
 if __name__ == "__main__":
