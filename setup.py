70eb450734133e963ced7d89d8394ca8144b8f26
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-10-17 14:36:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except:
 
 setup(
     # Self-descriptive entries which should always be present
-    name="malt",
+    name="malt.wangyq.net",
     author="Yuanqing Wang",
     author_email="wangyq@wangyq.net",
     description=short_description[0],
